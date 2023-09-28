5f337f50b087b4a67ed01ce8ccb34d1b72332b69
NursultanFront <nursultan020395@gmail.com>
2023-09-28 16:20:22 +0500

    feat: tailwind font-size configure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontSize: {
+      "text-xs": "0.75rem",
+      "text-sm": "0.875rem",
+      "text-base": "1rem",
+      "text-lg": "1.125rem",
+      "text-xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.5rem",
+      "4xl": "1.5rem",
+      "5xl": "2rem",
+      "6xl": "2.5rem",
+      "7xl": "5.625rem",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
