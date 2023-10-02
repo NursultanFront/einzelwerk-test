473c5c25ff606829b55bae5c0ae904f922557de7
NursultanFront <nursultan020395@gmail.com>
2023-10-02 14:53:05 +0500

    feat: accordion with show image

--- src/components/Project/ProjectWrapper.tsx
+++ src/components/Project/ProjectWrapper.tsx
@@ -2,20 +2,33 @@
 import { useState } from "react";
 import { list } from "@/text/accordion.json";
 import { AccordionWrapper } from "@/components/AccrodionWrapper/AccordionWrapper";
+import Image from "next/image";
 
 const ProjectWrapper = () => {
   const [showIndex, setShowIndex] = useState<number>(0);
 
-  const image = [];
+  const image = [
+    "/img/camera.png",
+    "/img/main.png",
+    "/img/right-img.png",
+    "/img/left-img.png",
+    "/img/camera.png",
+  ];
 
   const getIndex = (value: number) => {
     setShowIndex(value);
   };
 
   return (
-    <div className="flex">
-      <AccordionWrapper list={list} getIndex={getIndex}></AccordionWrapper>
-      <div className=""></div>
+    <div className="grid grid-cols-2 gap-24">
+      <AccordionWrapper list={list} getIndex={getIndex} />
+      <Image
+        src={image[showIndex]}
+        alt="ахахаххахх"
+        width={400}
+        height={400}
+        className="h-auto w-full rounded-3xl"
+      />
     </div>
   );
 };
